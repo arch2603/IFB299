73a4f404ea7ee66f2de16c9675fc8304ba2dc4b9
Raguletto <emmanuel@cyberize.com.au>
2015-09-15 13:46:18 +1000

    Removed App Files.

--- html/jobDetails.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<body>
-		<?php
-			print_r($_GET);
-			print_r($_POST);
-		?>
-	</body>
-</html>
--- html/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require 'include/functions.inc';
-	sessionTest();
-	
-	unset($_SESSION['userID']);
-	header("Location: $_SERVER[HTTP_REFERER]");
-?>
