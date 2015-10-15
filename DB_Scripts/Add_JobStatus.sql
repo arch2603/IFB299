5ce66fac267b6adab1891449bf03bb65d1d5e4ae
arch2603 <archiesua@gmail.com>
2015-10-15 15:31:42 +1000

    Merge remote-tracking branch 'refs/remotes/MAD-DEP/master' into PHPtestpages
    Conflicts:
    	.gitignore
    	Portfolio-1/David/jobRequest.php
    	Portfolio-1/Emmanuel/Artefact 2/registration.inc
    	html/home.php
    	html/include/alreadyRegistered.inc
    	html/include/functions.inc
    	html/include/processReg.inc
    	html/jobDetails.php
    	html/jobRequest.php
    	html/jobrequest.php
    	html/login.php
    	html/logout.php

---
+++ DB_Scripts/Add_JobStatus.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `mad_dep` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `mad_dep`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: 10.71.34.101    Database: mad_dep
+-- ------------------------------------------------------
+-- Server version	5.5.44-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `JobStatus`
+--
+
+DROP TABLE IF EXISTS `JobStatus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `JobStatus` (
+  `StatusID` int(11) NOT NULL AUTO_INCREMENT,
+  `StatusDescription` varchar(45) NOT NULL,
+  PRIMARY KEY (`StatusID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `JobStatus`
+--
+
+LOCK TABLES `JobStatus` WRITE;
+/*!40000 ALTER TABLE `JobStatus` DISABLE KEYS */;
+INSERT INTO `JobStatus` VALUES (1,'Open'),(2,'Scheduled'),(3,'Complete');
+/*!40000 ALTER TABLE `JobStatus` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-09 15:56:32
+
+ALTER TABLE `mad_dep`.`Jobs` 
+ADD COLUMN `JobStatus` INT NOT NULL DEFAULT 1 AFTER `JobDescription`;
